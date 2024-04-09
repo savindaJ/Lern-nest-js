b25e7ec185282bba744f03bd00d9feb96755109b
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-04-09 09:48:46 +0530

    add to genarate next order is

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { randomUUID } from 'crypto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/')
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    const s = randomUUID();
+    console.log(s);
+    return { nextId: s };
   }
 }
